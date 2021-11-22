3ce1cab9c495aeecdaca68ed01e296c8640fc020
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-11-22 12:43:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -20,11 +20,13 @@ inner join asset.subject s on cl.subject_id = s.id
 inner join asset.class_team ct on cl.id = ct.class_id
 inner join asset.team t on ct.team_id = t.id;`;
 //get all cards
-app.get("/kapital", async (req, res) => {
+app.get("/kapital", async(req, res) => {
     try {
         const displayAllCardData = await pool.query(sqlCode);
-        res.render('index', { displayAllCardData })
-        //res.json(displayAllCardData.rows)
+        //res.render('index', {displayAllCardData})
+
+        //seperate req, render singlecard.ejs
+        res.json(displayAllCardData.rows)
     } catch (err) {
         console.error(err.message);
     }
@@ -40,27 +42,29 @@ inner join asset.class_team ct on cl.id = ct.class_id
 inner join asset.team t on ct.team_id = t.id
 WHERE cl.id = ($1);`;
 //get a single card by id
-app.get("/kapital/:id", async (req, res) => {
+app.get("/kapital/:id", async(req, res) => {
     try {
         const { id } = req.params;
         const displaySingleCardData = await pool.query(sqlCodeId, [id]);
-        //res.render('index', {displaySingleCardData});
-        res.json(displaySingleCardData.rows);
+        res.render('index', {displaySingleCardData});
+        //res.json(displaySingleCardData.rows);
     } catch (err) {
         console.error(err.message);
     }
 });
 
 
+//put request needs to be different to update single card data //update every single document
+
 //update card - :id needs to match id name for destructuring {} value of id
-app.put("/kapital/:id", async (req, res) => {
+app.put("/kapital/:id", async(req, res) => {
     try {
         const { id } = req.params;
-        const { year } = req.body;
+        const { year } = req.body  //req.body.updateData ex. to use main.js fxn
         const { number } = req.body;
-        const { team } = req.body;
-        const { name } = req.body;
-        //if {name} was {descName} then correct way to write it is {descName: "aksjdflk"} etc...
+        const { team } = req.body
+        const { name } = req.body
+//if {name} was {descName} then correct way to write it is {descName: "aksjdflk"} etc...
         const updateSingleCardYrNum = await pool.query(`update asset."class" 
         set year = $1, number = $2 where asset."class".id = $3;`, [year, number, id]);
 
@@ -69,15 +73,17 @@ app.put("/kapital/:id", async (req, res) => {
         from asset.class_team, asset."class" 
         where asset.class_team.team_id = asset.team.id 
             and asset.class_team.class_id = asset."class".id 
-            and asset."class".id = $2;`, [team, id]);
+            and asset."class".id = $2;`, [team, id]); 
 
         const updateSingleCardName = await pool.query(`update asset.subject 
         set name = $1
         from asset."class" 
         where asset."class".subject_id = asset.subject.id 
-            and asset.subject.id = $2`, [name, id]);
-
-        res.json("row was updated");
+            and asset.subject.id = $2`, [name, id]); 
+        
+        //res.render('index', {updateSingleCardName, updateSingleCardYrNum, updateSingleCardTeam});
+        res.json('Update was succesfull!');
+        console.log("update worked");
     } catch (err) {
         console.error(err.message);
     }
@@ -89,25 +95,6 @@ app.listen(3000, () => {
     console.log("server running on port 3000");
 });
 
-
-// update asset."class" 
-// set year = 20190000, number = 170000
-// where asset."class".id = 1;
-// --where asset.card.class_id = asset."class".id;
-
-
-// update asset.team 
-// set name = 'LA lakersssssss'
-// from asset.class_team, asset."class" 
-// where asset.class_team.team_id = asset.team.id 
-// 	and asset.class_team.class_id = asset."class".id 
-// 	and asset."class".id = 1;
-
-// update asset.subject 
-// set name = 'RayJonJon Rondoooo'
-// from asset."class" 
-// where asset."class".subject_id = asset.subject.id 
-// 	and asset.subject.id = 1;
 
 
 
